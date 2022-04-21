aad37eaf267d57df33e35c8b47d0be15832a5448
Cally0611 <34480019+Cally0611@users.noreply.github.com>
2022-04-21 09:41:45 +0800

    Update DBOperations.cs

--- DBOperations.cs
+++ DBOperations.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OrlofOeev5.HelperClasses
+namespace DBOps.HelperClasses
 {
     public class DBOperations
     {
